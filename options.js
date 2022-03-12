62dface9815ed7cbc4eaa6d4ca5ea2063da5b5ae
Mehul Kumar <mehulsrivastava123@gmail.com>
2022-03-12 19:09:09 +0530

    block fn added to BLOCK btn.

--- options.js
+++ options.js
@@ -1,1 +1,16 @@
-console.log('connected');
+$(() => {
+    // elements of the HTML
+    $blockBtn = $('.btn');
+    $blockBtn.click(() => {
+        $urlToblock = $('#newurl').val();
+        block($urlToblock)
+        $('#newurl').val('');
+    });
+
+    // functions below
+    // function for the BLOCK button
+    function block(url) {
+        BLOCKER.data.blocked_urls[BLOCKER.data.blocked_urls.length] = url;
+        BLOCKER.methods.alert(url, 'add'); 
+    }
+});
